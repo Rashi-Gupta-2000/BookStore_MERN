1760c171a475b109c7770239700299acfb1c13e9
Latika Swarnkar <latika.swarnkar@innovaccer.com>
2022-05-02 13:37:00 +0530

    Dockerisation Done

--- Node/index.js
+++ Node/index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
-
+const DB = 'mongodb+srv://latika:123@cluster0.shk9r.mongodb.net/bookstore?retryWrites=true&w=majority'
 const cors = require("cors");
 const helmet = require("helmet");
 
@@ -24,7 +24,7 @@ app.get("/", (req, res) => {
 app.listen(4507, (e) => {
     console.log("Bookstore app listening on port 4500......")
 
-    mongoose.connect("mongodb://localhost/bookstore").then((result) => {
+    mongoose.connect(DB).then((result) => {
         console.log("Database Connected")
     }).catch((e) => {
         console.log("Database connection failed")
